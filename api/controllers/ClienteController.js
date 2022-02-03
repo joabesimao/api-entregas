36bbdb0edbc0d246a13371471c180a0b5c87477e
joabesimao <simaojoabecosta@gmail.com>
2022-02-03 07:38:16 -0300

    criar metodos CRUD

--- api/controllers/ClienteController.js
+++ api/controllers/ClienteController.js
@@ -11,7 +11,7 @@ class ClienteController {
     }
   }
 
-  static async pegarUmCliente(req, res) {
+  static async pegaUmCliente(req, res) {
     const { id } = req.params;
     try {
       const umCliente = await database.Clientes.findOne({
@@ -23,11 +23,35 @@ class ClienteController {
     }
   }
 
-  static async criarCliente(req, res) {
+  static async criaCliente(req, res) {
     const novoCliente = req.body;
     try {
       const novoClienteCriado = await database.Clientes.create(novoCliente);
       return res.status(200).json(novoClienteCriado);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async atualizaCliente(req, res) {
+    const { id } = req.params;
+    const novasInfos = req.body;
+    try {
+      await database.Clientes.update(novasInfos, { where: { id: Number(id) } });
+      const pessoaAtualizada = await database.Clientes.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(pessoaAtualizada);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async apagaCliente(req, res) {
+    const { id } = req.params;
+    try {
+      await database.Clientes.destroy({ where: { id: Number(id) } });
+      return res.status(200).json({ mensagem: `id ${id} deletado!` });
     } catch (error) {
       return res.status(500).json(error.message);
     }
